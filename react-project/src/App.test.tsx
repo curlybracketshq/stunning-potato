5d54d4018d054db2af0db14ba65a4600f35607af
Giovanni Cappellotto <potomak84@gmail.com>
2022-02-11 23:23:02 -0500

    Basic connection with MetaMask
    I've followed this example
    https://dev.to/rounakbanik/building-a-web3-frontend-with-react-340c
    to connect to the local hardhat node through metamask.
    
    In order to test you have to:
    
    1. Start the hardhat local node (`hardhat-project` directory)
    
    ```
    npx hardhat node
    ```
    
    2. Deploy the contract (`hardhat-project` directory)
    
    ```
    npx hardhat --network localhost run scripts/deploy.ts
    ```
    
    3. Keep track of the contract address after the deployment
    4. Start the Reach app (`react-project` directory)
    
    ```
    npm start
    ```
    
    5. Install MetaMask
    6. Setup the connection with the hardhat node to http://127.0.0.1:8545/
       (chain ID: 31337)
    7. Import a new account from the list printed when you start the
       hardhat node
    
    Example session:
    
    https://user-images.githubusercontent.com/153842/153696162-748bb1c2-5fb6-48bb-ac5d-e6450e6b0c94.mov

--- react-project/src/App.test.tsx
+++ react-project/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders some content', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/Stunning Potato/i);
+  expect(element).toBeInTheDocument();
 });
--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,24 +1,141 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import "./App.css";
+import StunningPotato from "./contracts/StunningPotato.json";
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      readonly REACT_APP_STUNNING_POTATO_CONTRACT_ADDRESS: string;
+      readonly NODE_ENV: "development" | "production" | "test";
+    }
+  }
+}
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  const checkWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.error("Make sure you have Metamask installed");
+      return;
+    }
+
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    if (accounts.length === 0) {
+      console.log("No authorized accounts");
+      return;
+    }
+
+    console.log("Accounts", accounts);
+    setCurrentAccount(accounts[0]);
+
+    console.log(
+      "Contract address",
+      process.env.REACT_APP_STUNNING_POTATO_CONTRACT_ADDRESS
+    );
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const stunningPotato = new ethers.Contract(
+      process.env.REACT_APP_STUNNING_POTATO_CONTRACT_ADDRESS,
+      StunningPotato.abi,
+      signer
+    );
+
+    const totalSupply = await stunningPotato.totalSupply();
+    console.log("Total supply", totalSupply.toString());
+    let index = ethers.BigNumber.from(0);
+    while (index.lt(totalSupply)) {
+      const tokenID = await stunningPotato.tokenByIndex(index);
+      console.log("Token at index", index.toString(), tokenID.toString());
+      index = index.add(1);
+    }
+  };
+
+  const connectWalletHandler = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.error("Make sure you have Metamask installed");
+      return;
+    }
+
+    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+    console.log("Accounts", accounts);
+    setCurrentAccount(accounts[0]);
+  };
+
+  const mintNftHandler = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.error("Make sure you have Metamask installed");
+      return;
+    }
+
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const stunningPotato = new ethers.Contract(
+      process.env.REACT_APP_STUNNING_POTATO_CONTRACT_ADDRESS,
+      StunningPotato.abi,
+      signer
+    );
+
+    console.log("Create frame");
+    const frameData = [];
+    // A valid frame is any 141 bytes array
+    for (let i = 0; i < 282; i++) {
+      frameData.push(Math.floor(Math.random() * 16).toString(16));
+    }
+    const tx = await stunningPotato.createFrame(
+      currentAccount,
+      `0x${frameData.join('')}`,
+      {
+        value: ethers.utils.parseEther("0.1"),
+      }
+    );
+
+    console.log("Mining...");
+    await tx.wait();
+
+    console.log("Done");
+  };
+
+  const connectWalletButton = () => {
+    return (
+      <button
+        onClick={connectWalletHandler}
+        className="cta-button connect-wallet-button"
+      >
+        Connect Wallet
+      </button>
+    );
+  };
+
+  const mintNftButton = () => {
+    return (
+      <button onClick={mintNftHandler} className="cta-button mint-nft-button">
+        Mint NFT
+      </button>
+    );
+  };
+
+  useEffect(() => {
+    checkWalletIsConnected();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-app">
+      <h1>Stunning Potato</h1>
+      <div>{currentAccount ? mintNftButton() : connectWalletButton()}</div>
     </div>
   );
 }
